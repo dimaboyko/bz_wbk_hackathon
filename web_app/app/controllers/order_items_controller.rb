d7453cde141f5630ee96b84c2dbbf09fe32bfc6a
Radek R <radek@wredny.net>
2017-05-21 00:30:53 +0200

    assign order via js (#12)

--- web_app/app/controllers/order_items_controller.rb
+++ web_app/app/controllers/order_items_controller.rb
@@ -3,12 +3,9 @@ class OrderItemsController < ApplicationController
 
   # POST
   def assign
-    client = User.find_by_name('Marcin')
-    order_item = OrderItem.find_by_name('Pizza')
+    @client = User.find_by_name('Marcin')
+    @order_item = OrderItem.find_by_name('Pizza')
 
-    OrderItemAssigner.perform(client: client, order_item: order_item)
-
-    contract = Contract.last # tmp
-    redirect_to contract_path(contract)
+    OrderItemAssigner.perform(client: @client, order_item: @order_item)
   end
 end
