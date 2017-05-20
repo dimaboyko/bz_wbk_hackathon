41358dad6411c9a85b753fadf31679e28b9f6c34
Dima Boyko <dima.boyko@infakt.pl>
2017-05-20 15:45:29 +0200

    fixed migrations

--- web_app/db/migrate/20170520133151_create_users.rb
+++ web_app/db/migrate/20170520133151_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :name
       t.string :available_amount, default: 500
-      t.string :integer
 
       t.timestamps
     end
--- web_app/db/migrate/20170520133528_create_transactions.rb
+++ web_app/db/migrate/20170520133528_create_transactions.rb
@@ -2,9 +2,7 @@ class CreateTransactions < ActiveRecord::Migration[5.0]
   def change
     create_table :transactions do |t|
       t.string :user_id
-      t.string :integer
       t.string :contract_id
-      t.string :integer
       t.integer :amount
 
       t.timestamps
