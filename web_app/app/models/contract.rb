8f1fd60b331096715dd918a0b3dcd1624315721a
Dima Boyko <dima.boyko@infakt.pl>
2017-05-20 16:04:45 +0200

    added keys

--- web_app/app/models/contract.rb
+++ web_app/app/models/contract.rb
@@ -1,4 +1,4 @@
 class Contract < ApplicationRecord
-  belongs_to :owner, class_name: "User"
-  belongs_to :client, class_name: "User"
+  belongs_to :owner, class_name: "User" ,foreign_key: "owner_id"
+  belongs_to :client, class_name: "User",foreign_key: "client_id"
 end
