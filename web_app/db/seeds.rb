8f7058974758f1317174f36472f93eb521ae67e3
Marcin Szoldrowski <marcin.szoldrowski@infakt.pl>
2017-05-20 17:38:29 +0200

    simple seeds

--- web_app/db/seeds.rb
+++ web_app/db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#deletion
+Order.delete_all
+OrderItem.delete_all
+User.delete_all
+
+#create Order
+order = Order.create
+
+#create OrderItems
+order.order_items.create(name: 'Piwo', amount: 5, quantity: 15)
+order.order_items.create(name: 'Golonka', amount: 20, quantity: 4)
+order.order_items.create(name: 'Kawa', amount: 10, quantity: 1)
+order.order_items.create(name: 'Orzeszki ziemne', amount: 6, quantity: 1)
+order.update_columns(total_amount: order.order_items.sum(&:amount))
+
+#create owner & client
+owner = User.create(name: 'Restauracja', available_amount: 1000)
+client = User.create(name: 'Jacek', available_amount: 500)
+
+#create Contract
+Contract.create!(owner_id: owner.id, client_id: client.id, order_id: order.id, status: 'NEW', total_amount:  order.total_amount, paid_amount: 0)
