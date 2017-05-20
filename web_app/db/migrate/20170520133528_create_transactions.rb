12dafac2d930e868e69a3cbaa40283610803e353
Dima Boyko <dima.boyko@infakt.pl>
2017-05-20 16:02:54 +0200

    fixed fileds

--- web_app/db/migrate/20170520133528_create_transactions.rb
+++ web_app/db/migrate/20170520133528_create_transactions.rb
@@ -1,8 +1,8 @@
 class CreateTransactions < ActiveRecord::Migration[5.0]
   def change
     create_table :transactions do |t|
-      t.string :user_id
-      t.string :contract_id
+      t.integer :user_id
+      t.integer :contract_id
       t.integer :amount
 
       t.timestamps
